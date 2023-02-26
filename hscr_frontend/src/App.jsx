a76ec99e873e5b42329dce676ec0353a92602833
Shuoan Lee <leeshuoan38@gmail.com>
2023-02-26 09:37:23 +0800

    readded useState import

--- hscr_frontend/src/App.jsx
+++ hscr_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import NinjaAppBar from "./NinjaAppBar";
 import CallIcon from "@mui/icons-material/Call";
